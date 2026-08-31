9cb75675e09144782a131ec8dc24a1ee26d8b9d9
Abdirahman-Calasow <mmsomtech@gmail.com>
2026-08-31 00:35:59 -0700

    added exr 07

---
+++ EXERCISE07/scripts.js
@@ -0,0 +1,12 @@
+let colors=["Red", "Blue", "Yellow","Green"];
+
+console.log(colors)
+
+ colors.push("black");
+ console.log(colors)
+ colors.pop("black");
+ console.log(colors)
+ colors.shift("black");
+ console.log(colors)
+ colors.unshift("black");
+ console.log(colors)
