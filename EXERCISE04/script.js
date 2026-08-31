8f3d7f49e47e321ad333c2f9e123eacea6c0a305
Abdirahman-Calasow <mmsomtech@gmail.com>
2026-08-30 23:49:09 -0700

    added exe 4

---
+++ EXERCISE04/script.js
@@ -0,0 +1,26 @@
+function add (a,b){
+
+    return a+b;
+
+}
+
+console.log( add(10,30));
+
+console.log( add(90,30));
+console.log( add(10,50));
+ 
+
+// Expretions function
+
+console.log("---------- expretion fun-------------")
+
+const addd = function(a,b){
+
+    return a+b;
+
+}
+
+console.log( addd(10,30));
+
+console.log( addd(90,30));
+console.log( addd(10,50));
---
+++ main.js
@@ -0,0 +1,7 @@
+ function add(a,b){
+     return a+b;
+ }
+
+let result= add(4,4)
+
+console.log(result / 2)
