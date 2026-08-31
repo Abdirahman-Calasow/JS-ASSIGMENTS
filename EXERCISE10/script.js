f2ecba6ae5787802271bb185df71cf4085e3ee63
Abdirahman-Calasow <mmsomtech@gmail.com>
2026-08-31 06:23:10 -0700

    added exr10

---
+++ EXERCISE10/script.js
@@ -0,0 +1,16 @@
+ let library = [
+
+    {
+        tittle : "king",
+        author : "calsow",
+        year : 2000
+    },
+    {
+        tittle : "buutk",
+        author : "xamuuda",
+        year : 1999
+    }
+ ]
+
+ console.log(library)
+ console.log(library[0])
