6f6d6e7e8dd5b71c4601723c05abf71969cc525e
Abdirahman-Calasow <mmsomtech@gmail.com>
2026-08-31 01:03:56 -0700

    added exr 09

---
+++ EXERCISE09/script.js
@@ -0,0 +1,13 @@
+
+
+ let car = {
+    make: "TOYOTO",
+    model: "v6v",
+    year:  1999,
+    start: function(){
+        console.log("The car has started", this.make) ;
+    }
+     
+ }
+
+ console.log(car);
