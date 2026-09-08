1525d21ca006a8fed0e1964cbaae97a372d20eac
Abdirahman-Calasow <mmsomtech@gmail.com>
2026-09-08 01:00:00 -0700

    added exerise 33

---
+++ EXERCISE33/main.js
@@ -0,0 +1,19 @@
+
+//  function changeContect(){
+//      const head = document.querySelector("#head");
+   
+//     head.textContent= "welcpme to the dom" 
+//      `
+//  };
+  
+
+ function changeContect (){
+    const head = document.querySelector("#head")
+
+     head.textContent= "welcome dom manupuliton"
+ }
+ function changeElemnt (){
+    const text = document.querySelector (".text")
+
+     text.textContent= 'This content has been <strong>updated</strong>.';
+ }
